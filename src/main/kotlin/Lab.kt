a5163d716496d9625f7136b7eff8567e7e11134c
Vadim170 <HL#farcry170>
2019-10-22 19:08:56 +0300

    Initial commit lab8_5

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,5 +1,13 @@
-//import kotlinx.coroutines
 
 fun main() {
+    listOf("three", "two", "one").forEach lit@{
+        if(it == "one") {
+            return@lit
+            // т.к. forEach inline, то "return" не вызовет ошибку, он называется нелокальным
+            // "retutn" без метки вызовет завершение функции main, а необходимо выйти из лямбды, для этого
+            // используем "return@lit" чтобы вернуться к метке lit@
+        }
+        println(it)
+    }
     println("boom!")
 }
