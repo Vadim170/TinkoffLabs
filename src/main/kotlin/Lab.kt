d1d6163f750d580d0d9e4d4639d249096e9c24db
CODE <Vadim.17.0@yandex.ru>
2019-11-01 08:33:30 +0300

    initial commit lab10_3

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,49 +1,10 @@
-package ru.tinkoff.lab8_6
+package ru.tinkoff.lab10_3
 
-import kotlinx.coroutines.*
-import java.time.Duration
-
-suspend fun main() {
-    val pets = ArrayList<Pet>()
-    pets.apply {
-        add(Pet("Пёс", Duration.ofSeconds(1)))
-        add(Pet("Вася", Duration.ofSeconds(4)))
-        add(Pet("Бобик", Duration.ofSeconds(3)))
-        add(Pet("Тотоша", Duration.ofSeconds(1)))
-        add(Pet("Барс", Duration.ofSeconds(2)))
-    }
-
-    // Задание 1
-    println("Задание 1:")
-    runBlocking {
-        pets.forEach {
-            launch { it.eat() }
-        }
-    }
-    println("Животные накормлены")
-
-    // Задание 2
-    println("Задание 2:")
-    GlobalScope.launch {
-        val result = async {
-            pets.forEach {
-                launch { it.eat() }
-            }
-        }
-        launch{
-            result.await()
-            println("Животные накормлены")
-        }
-    }
-    println("Еда роздана")
-    Thread.sleep(5000) // Даю время для завершения фонового потока корутины.
-}
-
-class Pet(
-    val nickName: String,
-    val mealDuration: Duration = Duration.ZERO) {
-    suspend fun eat() {
-        delay(mealDuration.toMillis())
-        println("$nickName покушал")
-    }
+fun main() {
+    val listSmeshariki = listOf("Крош","Ежик","Нюша","Бараш")
+    val setSmeshariki = setOf("Крош","Ежик","Нюша","Бараш")
+    println("Список смешариков:")
+    listSmeshariki.forEach { println(it) }
+    println("Множество смешариков:")
+    setSmeshariki.forEach { println(it) }
 }
