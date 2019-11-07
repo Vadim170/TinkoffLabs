03e364f0efff66da5abe96a616e139853a07d375
CODE <Vadim.17.0@yandex.ru>
2019-11-07 18:53:36 +0300

    first commit lab12_7

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,10 +1,10 @@
-package ru.tinkoff.lab12_5
+package ru.tinkoff.lab12_7
 
 fun main() {
     val names = listOf("Хрюша","Степаша","Филя","Гуля")
-    val isExistNamesLengthMoreSeven = names.any { it.length == 7 }
-    if(isExistNamesLengthMoreSeven)
-        println("Есть имена длинной семь символов")
-    else
-        println("Нет имен длинной семь символов")
+    val helloNamesLengthLessFive = names.asSequence()
+        .filter { it.length < 5 }
+        .map { "Привет $it" }
+        .toList()
+    println(helloNamesLengthLessFive)
 }
