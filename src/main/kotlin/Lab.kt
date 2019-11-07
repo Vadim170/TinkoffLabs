6a75893093c4cdc542a2bd024df2315cd42c9892
CODE <Vadim.17.0@yandex.ru>
2019-11-07 17:53:28 +0300

    First commit lab12_2

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,28 +1,7 @@
-package ru.tinkoff.lab11_2_2
+package ru.tinkoff.lab12_2
 
 fun main() {
-    val count = readLine()?.toInt()?:0
-    val inputStr = readLine() ?: ""
-
-    val nodes = mutableMapOf<Int,Int>()
-    inputStr.split(" ").forEachIndexed { index, parent -> nodes.put(index, parent.toInt()) }
-
-    val findNodes = mutableMapOf<Int,Int>()
-    val lastestIndexes = mutableSetOf<Int>()
-    var height = 0
-    findNodes.putAll(nodes.filterValues { it == -1 })
-    while (findNodes.isNotEmpty()) {
-        val temp = mutableMapOf<Int,Int>()
-        temp.putAll(findNodes)
-        findNodes.clear()
-        temp.forEach {
-            val level = it.key
-            if(!lastestIndexes.contains(level) && lastestIndexes.size < count) {
-                lastestIndexes.add(level)
-                findNodes.putAll(nodes.filterValues { it == level })
-            }
-        }
-        ++height
-    }
-    println(height)
+    val names = listOf("Хрюша","Степаша","Филя","Гуля")
+    val helloNames = names.map{"Привет $it"}
+    println(helloNames)
 }
