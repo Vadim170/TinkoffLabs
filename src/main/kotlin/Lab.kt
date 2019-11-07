54f658e5ddda0ecb97407487abd6d07018d00b5a
CODE <Vadim.17.0@yandex.ru>
2019-11-07 18:24:13 +0300

    First commit lab12_4

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,7 +1,16 @@
-package ru.tinkoff.lab12_3
+package ru.tinkoff.lab12_4
 
 fun main() {
     val names = listOf("Хрюша","Степаша","Филя","Гуля")
-    val namesLengthLessSix = names.filter { it.length < 6 }
-    println(namesLengthLessSix)
+    var maxLength = 0
+    names.forEach {
+        if(it.length > maxLength)
+            maxLength = it.length
+    }
+    val namesMaxLength = names.find { it.length == maxLength }
+    println(namesMaxLength)
+    // Тут логичнее применить не find, а maxBy :
+    // names.maxBy { it.length }
+    // А то приходится сначала искать максимальную длину пробегаясь по листу, потом пробегаться второй раз в find,
+    // сравнивая длину с найденной ранее максимальной.
 }
