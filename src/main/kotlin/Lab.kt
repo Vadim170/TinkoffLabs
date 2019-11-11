444380c2854b0a905832d54061cc9a72a1e0aa2b
CODE <Vadim.17.0@yandex.ru>
2019-11-11 18:04:35 +0300

    Some commit lab11_2_3

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -16,46 +16,66 @@ fun main() {
 
         // Мы получили новое время, теперь проматываем все операции процессора до этого момента
         while(taskQueue.isNotEmpty()) {
-            if(Processor.isFree(arrivalTime))
-                Processor.addTask(arrivalTime, taskQueue.poll())
-            else {
-                taskQueue.poll()
-                Processor.onSkipTask()
-            }
+            if(Processor.isHaveTimeToDo(arrivalTime, taskQueue.peek()))
+                Processor.doTask(arrivalTime, taskQueue.poll())
+            else
+                break
         }
 
+        if(taskQueue.size >= size /*|| Processor.isInPast(arrivalTime)*/)
+            Processor.onSkipTask() // Буффер переполнен
+        else
+            taskQueue.add(task)
+    }
 
-        if(taskQueue.size >= size) {
-            Processor.onSkipTask()
-            continue
-        } // Буффер переполнен
-        else(taskQueue.add(task))
+    while(taskQueue.isNotEmpty()) {
+        Processor.doTask(0, taskQueue.poll())
     }
 }
 
-class Task(val arrivalTime: Int, val duration: Int)
+class Task(val arrivalTime: Int, val duration: Int) : Comparable<Task> {
+    override fun compareTo(other: Task): Int {
+        return arrivalTime.compareTo(other.arrivalTime)
+    }
+}
+
 
 object Processor {
-    private var timeFormulationTask = 0
+    //private var actualTask: Task? = null
+    //private var timeFormulationTask = 0
     private var timeRelease = 0
-    private var actualTask: Task? = null
+
     fun isFree(nowTime: Int) : Boolean {
         return nowTime >= timeRelease
     }
 
-    fun addTask(nowTime: Int, task: Task) {
-        if (!isFree(nowTime)) return
+    /*fun addTask(limitTime: Int, task: Task) {
+        if (!isFree(limitTime)) return // Если текущая задача
         if (actualTask!=null) onDoActualTask()
         actualTask = task
-        timeFormulationTask = nowTime
-        timeRelease = nowTime + task.duration
+        timeFormulationTask = limitTime
+        timeRelease = limitTime + task.duration
     }
 
     fun onDoActualTask() {
         println(timeFormulationTask)
-    }
+    }*/
 
     fun onSkipTask() {
         println(-1)
+    }
+
+    fun isHaveTimeToDo(arrivalTime: Int, task: Task): Boolean {
+        return isFree(timeRelease + task.duration)
+    }
+
+    fun doTask(arrivalTime: Int, task: Task) {
+        if (!isHaveTimeToDo(arrivalTime, task)) return
+        println(timeRelease) // Время окончания обработки прошлой задачи оно же время начала следующей
+        timeRelease += arrivalTime + task.duration
+    }
+
+    fun isInPast(arrivalTime: Int): Boolean {
+        return !isFree(arrivalTime)
     }
 }
