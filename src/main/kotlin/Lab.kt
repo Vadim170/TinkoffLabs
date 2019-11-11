a2848c70a8e92e2622e88baa461a25ffc97c3c26
CODE <Vadim.17.0@yandex.ru>
2019-11-11 12:29:53 +0300

    For show teacher

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -4,28 +4,31 @@ import java.util.*
 
 class Node(val value: Int, val parent: Int)
 
-val time = System.nanoTime()
 val nodes = mutableListOf<Node>()
 fun main() {
-    val cin = Scanner(System.`in`)
+    /*val cin = Scanner(System.`in`)
     val count = cin.nextInt()
-    //val count = readLine()?.toInt()?:0
     val root = mutableListOf<Int>()
     repeat(count) { index ->
         val parent = cin.nextInt()
         val node = Node(index, parent)
         nodes.add(node)
         if(node.parent == -1) root.add(node.value)
+    }*/
+
+    val count = readLine()?.toInt()?:0
+    val inputStr = readLine() ?: ""
+    val time = System.nanoTime()
+    println(System.nanoTime() - time)
+    val splitedStr = inputStr.split(' ')
+    println(System.nanoTime() - time)
+    val root = mutableListOf<Int>()
+    splitedStr.forEachIndexed { index, parent ->
+        val node = Node(index, parent.toInt())
+        nodes.add(node)
+        if(node.parent == -1) root.add(node.value)
     }
-        //readLine() ?: ""
-    //println(System.nanoTime() - time)
-    //val splitedStr = inputStr.split(' ')
-    //println(System.nanoTime() - time)
-//    splitedStr.forEachIndexed { index, parent ->
-//        val node = Node(index, parent.toInt())
-//        nodes.add(node)
-//        if(node.parent == -1) root.add(node.value)
-//    }
+
     println(calcHeightOfNodes(root))
     println(System.nanoTime() - time)
 }
