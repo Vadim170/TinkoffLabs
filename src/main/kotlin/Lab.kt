e00e35f050f2f53f731c131f447a7a4c6323310a
CODE <Vadim.17.0@yandex.ru>
2019-11-13 19:33:17 +0300

    Some commit

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,6 +1,7 @@
 package ru.tinkoff.lab11_2_3
 
 import java.util.*
+import java.util.stream.Stream
 
 /*
 2 8
@@ -34,6 +35,24 @@ import java.util.*
 1 3
 
 Ответ: 0 0 0 1 2 -1 -1 -1
+
+1 5
+999999 1
+1000000 0
+1000000 1
+1000000 0
+1000000 0
+
+Ответ: 999999 1000000 1000000 -1 -1
+
+4 5
+0 7
+2 7
+4 7
+6 7
+21 7
+
+Ответ? 7 14 21 28 35
  */
 
 fun main() {
@@ -41,75 +60,51 @@ fun main() {
     val sizeQueue = cin.nextInt()
     val countPockets = cin.nextInt()
     val taskQueue = PriorityQueue<Task>(sizeQueue)
+    var prevTask = Task(0,0)
     for (index in 0 until countPockets) {
         val arrivalTime = cin.nextInt()
         val duration = cin.nextInt()
         val task = Task(arrivalTime, duration)
         // Мы получили новое время, теперь проматываем все операции процессора до этого момента
         while(taskQueue.isNotEmpty()) {
-            if(Processor.isFinished(arrivalTime, taskQueue.peek()))
+            if(taskQueue.peek().isFinished(arrivalTime))
                 taskQueue.poll()
-            /*else if(Processor.isHaveTimeToDo(arrivalTime, taskQueue.peek()))
-                Processor.doTask(arrivalTime, taskQueue.peek())*/
             else
                 break
         }
-        if(taskQueue.size >= sizeQueue /*|| Processor.isInPast(arrivalTime)*/)
+        if(taskQueue.size >= sizeQueue)
             Processor.onSkipTask() // Буффер переполнен
         else {
             taskQueue.add(task)
-            Processor.doTask(arrivalTime, task)
+            Processor.doTask(arrivalTime, task, prevTask)
+            prevTask = task
         }
     }
-    /*while(taskQueue.isNotEmpty()) {
-        /*if(Processor.isFinished(Processor.timeRelease+1, taskQueue.peek()))
-            taskQueue.poll()
-        else*/
-            Processor.doTask(0, taskQueue.poll())
-    }*/
 }
 
 object Processor {
-    private var actualTask: Task? = null
-    //private var timeFormulationTask = 0
-    var timeRelease = 0
+    var outStream : Stream = System.out
+    fun onSkipTask() = println(-1)
+    private fun onDoTask(time: Int) = println(time)
 
-    fun isFree(nowTime: Int) : Boolean {
-        return nowTime + (actualTask?.duration ?: 0) >= timeRelease
-    }
-
-    fun onSkipTask() {
-        println(-1)
-    }
-
-    fun isHaveTimeToDo(arrivalTime: Int, task: Task): Boolean {
-        return isFree(arrivalTime/*timeRelease + task.duration*/)
-    }
-
-    fun doTask(arrivalTime: Int, task: Task) {
-        //if (!isHaveTimeToDo(arrivalTime, task)) return
-        if(timeRelease < arrivalTime)
-            println(arrivalTime)
+    fun doTask(arrivalTime: Int, task: Task, prevTask: Task) {
+        onDoTask(
+            if(prevTask.timeRelease < arrivalTime) arrivalTime
+            else prevTask.timeRelease // Время окончания обработки прошлой задачи оно же время начала следующей
+        )
+        if(prevTask.timeRelease < arrivalTime)
+            task.timeRelease = arrivalTime + task.duration
         else
-            println(timeRelease) // Время окончания обработки прошлой задачи оно же время начала следующей
-        //actualTask = task
-        if(timeRelease < arrivalTime)
-            timeRelease = arrivalTime + task.duration
-        else
-            timeRelease += /*arrivalTime*/ + task.duration
-    }
-
-    fun isInPast(arrivalTime: Int): Boolean {
-        return !isFree(arrivalTime)
-    }
-
-    fun isFinished(arrivalTime: Int, task: Task): Boolean {
-        return timeRelease + task.duration <= arrivalTime
+            task.timeRelease = prevTask.timeRelease + task.duration
     }
 }
 
 class Task(val arrivalTime: Int, val duration: Int) : Comparable<Task> {
+    var timeRelease = 0
     override fun compareTo(other: Task): Int {
         return arrivalTime.compareTo(other.arrivalTime)
+    }
+    fun isFinished(arrivalTime: Int) : Boolean {
+        return timeRelease <= arrivalTime
     }
 }
