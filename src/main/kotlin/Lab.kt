8fe42ff19787edab606b4ea6c2ebe1533813032a
CODE <Vadim.17.0@yandex.ru>
2019-11-19 20:49:08 +0300

    First commit lab15_2

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,10 +1,5 @@
-package ru.tinkoff.lab12_7
+package ru.tinkoff.lab15_2
 
 fun main() {
-    val names = listOf("Хрюша","Степаша","Филя","Гуля")
-    val helloNamesLengthLessFive = names.asSequence()
-        .filter { it.length < 5 }
-        .map { "Привет $it" }
-        .toList()
-    println(helloNamesLengthLessFive)
+    listOf("Хрюша", "Степаша", "Филя", "Гуля").firstOrNull { it.length > 6}
 }
