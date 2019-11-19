15ae71c9352b9bd0b82a1378f27e452f98294fc1
CODE <Vadim.17.0@yandex.ru>
2019-11-19 22:17:59 +0300

    First commit refactoring lab5_7

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,44 +1,66 @@
-package ru.tinkoff.lab7_5
+package ru.tinkoff.lab5_7
+
+import com.google.common.collect.ComparisonChain
+import java.lang.StringBuilder
+
+data class Person (
+    val lastName: String,
+    val firstName: String,
+    val age: Int,
+    val mailId: Int,
+    val phoneNumber: String
+) {
+    companion object : Comparator<Person>{
+        override fun compare(o1: Person?, o2: Person?): Int {
+            if(o1 == null || o2 == null) return 0
+            return ComparisonChain.start()
+                .compare(o1.lastName, o2.lastName)
+                .compare(o1.firstName, o2.firstName)
+                .compare(o1.age, o2.age)
+                .compare(o1.mailId, o2.mailId)
+                .compare(o1.phoneNumber, o2.phoneNumber)
+                .result()
+        }
+    }
+}
+
+object PersonsComparator : Comparator<Person> {
+    override fun compare(o1: Person?, o2: Person?): Int {
+        if(o1 == null || o2 == null) return 0
+        return ComparisonChain.start()
+            .compare(o1.lastName, o2.lastName)
+            .compare(o1.firstName, o2.firstName)
+            .compare(o1.age, o2.age)
+            .compare(o1.mailId, o2.mailId)
+            .compare(o1.phoneNumber, o2.phoneNumber)
+            .result()
+    }
+}
+
+fun stringComparePersons(person1: Person, person2: Person, comparator: Comparator<Person>): String {
+    val isIdentical = comparator.compare(person1, person2) == 1
+    return "${person1.firstName} ${if (isIdentical) "identical" else "not identical"} ${person2.firstName}"
+}
+
+fun compareThreePerson(
+    person1: Person,
+    person2: Person,
+    person3: Person,
+    comparator: Comparator<Person>
+)  = StringBuilder()
+        .appendln(stringComparePersons(person1,person2, comparator))
+        .appendln(stringComparePersons(person1,person3, comparator))
+        .append(stringComparePersons(person2,person3, comparator))
+        .toString()
 
 fun main() {
-    // Создадим хозяина Pet
-    val pet = Pet("Mary", 3, Sex.FEMALE)
-    val owner = Owner(pet)
-    owner.feed()
-    // Создадим хозяина Dog
-    val dog = Dog("Charly", 1)
-    val dogOwner = Owner(dog)
-    dogOwner.feed()
-}
-
-enum class Sex {
-    MALE, FEMALE
-}
-
-open class Pet(
-    val nickname: String,
-    val age: Int,
-    val sex: Sex? = null
-)
-
-class Dog(
-    nickname: String,
-    age: Int,
-    sex: Sex? = null
-) : Pet(nickname, age, sex)
-
-class Cat(
-    nickname: String,
-    age: Int,
-    sex: Sex? = null
-) : Pet(nickname, age, sex)
-
-fun Pet.isAddult() =
-    age >= 2
-
-val Pet.respectableNickname: String
-    get() = "Dear ${this.nickname}"
-
-class Owner<T : Pet>(val pet: T) {
-    fun feed() = println("Домашнее животное ${pet.nickname} накормлено")
+    val person1 = Person("Иванов", "Семён", 17, 330045, "+79209826345")
+    val person2 = Person("Алёхин", "Виктор", 23, 330045, "+79219821315")
+    val person3 = Person("Иванов", "Семён", 17, 330045, "+79209826345")
+    val output = StringBuilder()
+    output.appendln("Compare by PersonsComparator object:")
+    output.appendln(compareThreePerson(person1, person2, person3, PersonsComparator))
+    output.appendln("Compare by companion object:")
+    output.appendln(compareThreePerson(person1, person2, person3, Person))
+    println(output)
 }
