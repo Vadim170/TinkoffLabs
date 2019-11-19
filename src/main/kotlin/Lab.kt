9e3edb761e8d478f1b60edf4f996b5ca2f0cfd65
CODE <Vadim.17.0@yandex.ru>
2019-11-19 21:22:34 +0300

    First commit refactoring lab7_5

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,5 +1,44 @@
-package ru.tinkoff.lab15_2
+package ru.tinkoff.lab7_5
 
 fun main() {
-    listOf("Хрюша", "Степаша", "Филя", "Гуля").firstOrNull { it.length > 6 }
+    // Создадим хозяина Pet
+    val pet = Pet("Mary", 3, Sex.FEMALE)
+    val owner = Owner(pet)
+    owner.feed()
+    // Создадим хозяина Dog
+    val dog = Dog("Charly", 1)
+    val dogOwner = Owner(dog)
+    dogOwner.feed()
+}
+
+enum class Sex {
+    MALE, FEMALE
+}
+
+open class Pet(
+    val nickname: String,
+    val age: Int,
+    val sex: Sex? = null
+)
+
+class Dog(
+    nickname: String,
+    age: Int,
+    sex: Sex? = null
+) : Pet(nickname, age, sex)
+
+class Cat(
+    nickname: String,
+    age: Int,
+    sex: Sex? = null
+) : Pet(nickname, age, sex)
+
+fun Pet.isAddult() =
+    age >= 2
+
+val Pet.respectableNickname: String
+    get() = "Dear ${this.nickname}"
+
+class Owner<T : Pet>(val pet: T) {
+    fun feed() = println("Домашнее животное ${pet.nickname} накормлено")
 }
