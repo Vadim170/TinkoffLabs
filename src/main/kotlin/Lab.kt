a603f942460609693ba09adc6816afba8751f705
CODE <Vadim.17.0@yandex.ru>
2019-11-19 23:25:57 +0300

    First commit refactoring lab8_6

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -1,66 +1,43 @@
-package ru.tinkoff.lab5_7
+package ru.tinkoff.lab8_6
 
-import com.google.common.collect.ComparisonChain
-import java.lang.StringBuilder
+import kotlinx.coroutines.*
+import java.time.Duration
 
-data class Person(
-    val lastName: String,
-    val firstName: String,
-    val age: Int,
-    val mailId: Int,
-    val phoneNumber: String
-) {
-    companion object : Comparator<Person> {
-        override fun compare(o1: Person?, o2: Person?): Int {
-            if (o1 == null || o2 == null) return 0
-            return ComparisonChain.start()
-                .compare(o1.lastName, o2.lastName)
-                .compare(o1.firstName, o2.firstName)
-                .compare(o1.age, o2.age)
-                .compare(o1.mailId, o2.mailId)
-                .compare(o1.phoneNumber, o2.phoneNumber)
-                .result()
+suspend fun main() {
+    val pets = arrayListOf(
+        Pet("Пёс", Duration.ofSeconds(1)),
+        Pet("Вася", Duration.ofSeconds(4)),
+        Pet("Бобик", Duration.ofSeconds(3)),
+        Pet("Тотоша", Duration.ofSeconds(1)),
+        Pet("Барс", Duration.ofSeconds(2))
+    )
+
+    // Задание 1
+    println("Задание 1:")
+    runBlocking {
+        pets.forEach {
+            launch { it.eat() }
         }
     }
-}
+    println("Животные накормлены")
 
-object PersonsComparator : Comparator<Person> {
-    override fun compare(o1: Person?, o2: Person?): Int {
-        if (o1 == null || o2 == null) return 0
-        return ComparisonChain.start()
-            .compare(o1.lastName, o2.lastName)
-            .compare(o1.firstName, o2.firstName)
-            .compare(o1.age, o2.age)
-            .compare(o1.mailId, o2.mailId)
-            .compare(o1.phoneNumber, o2.phoneNumber)
-            .result()
+    // Задание 2
+    println("Задание 2:")
+    val globalScope = GlobalScope.async {
+        pets.forEach {
+            launch { it.eat()}
+        }
     }
+    println("Еда роздана")
+    globalScope.await()
+    println("Животные накормлены")
 }
 
-fun stringComparePersons(person1: Person, person2: Person, comparator: Comparator<Person>): String {
-    val isIdentical = comparator.compare(person1, person2) == 1
-    return "${person1.firstName} ${if (isIdentical) "identical" else "not identical"} ${person2.firstName}"
-}
-
-fun compareThreePerson(
-    person1: Person,
-    person2: Person,
-    person3: Person,
-    comparator: Comparator<Person>
-) = StringBuilder()
-    .appendln(stringComparePersons(person1, person2, comparator))
-    .appendln(stringComparePersons(person1, person3, comparator))
-    .append(stringComparePersons(person2, person3, comparator))
-    .toString()
-
-fun main() {
-    val person1 = Person("Иванов", "Семён", 17, 330045, "+79209826345")
-    val person2 = Person("Алёхин", "Виктор", 23, 330045, "+79219821315")
-    val person3 = Person("Иванов", "Семён", 17, 330045, "+79209826345")
-    val output = StringBuilder()
-    output.appendln("Compare by PersonsComparator object:")
-    output.appendln(compareThreePerson(person1, person2, person3, PersonsComparator))
-    output.appendln("Compare by companion object:")
-    output.appendln(compareThreePerson(person1, person2, person3, Person))
-    println(output)
+class Pet(
+    val nickName: String,
+    val mealDuration: Duration = Duration.ZERO) {
+    suspend fun eat() {
+        delay(mealDuration.toMillis())
+        println("$nickName покушал")
+    }
 }
