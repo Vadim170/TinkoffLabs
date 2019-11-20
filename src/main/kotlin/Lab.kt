6487d62728fbca667d5ee2e236dd1876dfcf9045
CODE <Vadim.17.0@yandex.ru>
2019-11-20 13:07:05 +0300

    add comment

--- src/main/kotlin/Lab.kt
+++ src/main/kotlin/Lab.kt
@@ -2,7 +2,7 @@ package ru.tinkoff.lab11_2_2
 
 import java.util.*
 
-val parents = mutableMapOf<Int, MutableList<Int>>()
+val parents = mutableMapOf<Int, MutableList<Int>>() // Key - родитель; Value - список его детей
 
 fun main() {
     val cin = Scanner(System.`in`)
@@ -10,7 +10,7 @@ fun main() {
     repeat(count) { value ->
         val parent = cin.nextInt()
         if(parents.containsKey(parent)) parents[parent]?.add(value) // Если список с детьми этого родителя уже есть в мапе, то добавим ребенка
-        else parents[parent] = mutableListOf(value) // Если родителя ещё нет в мапе, то создадим элемент и список с обним ребёнком
+        else parents[parent] = mutableListOf(value) // Если родителя ещё нет в мапе, то создадим элемент и список с одним ребёнком
     }
     parents[-1]?.let {
         println(calcHeightOfNodes(it))
